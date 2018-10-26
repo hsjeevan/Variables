1e42a44514a4b9cb9cc29e674f0fd6d7dbfefd00
hsjeevan <hsjeevan07@gmail.com>
2018-10-26 16:32:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var recipeName = "Creamy squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+
+console.log('Recipe details' +"\n"+ '--------------'+"\n"+
+'Recipe Name: ' + recipeName+"\n"+ 
+'Preparation Time: ' + preparationTime+"\n"+ 
+'Recipe By: ' + recipeBy+"\n"+
+'Is Vegetarian Dish: ' + isVegetarian+"\n"+
+'calories: ' + calories+"\n"
+);
+
+var preparationTime = 60;
+var recipeBy = "Jimmy Cutter George";
+
+console.log('Recipe details after update' +"\n"+ '--------------'+"\n"+
+'Recipe Name: ' + recipeName+"\n"+ 
+'Preparation Time: ' + preparationTime+"\n"+ 
+'Recipe By: ' + recipeBy+"\n"+
+'Is Vegetarian Dish: ' + isVegetarian+"\n"+
+'calories: ' + calories+"\n"
+);
